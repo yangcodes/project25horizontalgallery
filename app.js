a9548d8e98a3c297ce2416dbe8ccf02292274d66
Jenny Lin <misslinyang91@gmail.com>
2021-08-28 17:10:43 +1000

    created gallery scroll on mousemove

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const slider = document.querySelector(".container");
 let isDown = false;
 let startx;
-let scrollLeft;
+let scrollToLeft;
 
 slider.addEventListener("mousedown", function (e) {
   isDown = true;
   slider.classList.add("active");
 
-  startx = e.pageX;
+  startx = e.pageX - slider.offsetLeft;
+  scrollToLeft = slider.scrollLeft;
 });
 
 slider.addEventListener("mouseup", function () {
@@ -18,4 +19,13 @@ slider.addEventListener("mouseup", function () {
 slider.addEventListener("mouseleave", function () {
   isDown = false;
   slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", function (e) {
+  if (!isDown) return;
+  e.preventDefault();
+
+  const distanceX = e.pageX - slider.offsetLeft;
+  const walk = distanceX - startx;
+  slider.scrollLeft = scrollToLeft - walk;
 });
